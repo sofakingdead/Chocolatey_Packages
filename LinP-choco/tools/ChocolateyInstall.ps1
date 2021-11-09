6f332d719e97930bab81bde9b2f0e20ba4f636e7
AngusMcGyver <56880439+AngusMcGyver@users.noreply.github.com>
2021-11-09 15:05:27 +0100

    Update ChocolateyInstall.ps1
    update install script for 4.3.1

--- LinP-choco/tools/ChocolateyInstall.ps1
+++ LinP-choco/tools/ChocolateyInstall.ps1
@@ -1,13 +1,41 @@
 $ErrorActionPreference = 'Stop'
- 
-$packageArgs = @{
-  packageName            = 'Linphone'
-  fileType               = 'EXE'
-  url                    = 'http://www.linphone.org/releases/windows/Linphone-4.1.1-win32.exe'
-  checksum               = 'BB7B7B9B6B620BD15740CCE9E176F3342F6C8B1527830697A608E6FEFF08BDF3'
-  checksumType           = 'sha256'
-  silentArgs             = '/S'
-  validExitCodes         = @(0)
-  softwareName           = 'Linphone *'
+
+$LinphoneActive = Get-Process Linphone -ErrorAction SilentlyContinue
+if($LinphoneActive -eq $null) {
+	# the silent installer won't start the uninstaller of the old version in silent mode, so do it manually...
+	$packageArgsUnInstall = @{
+	packageName            = $env:ChocolateyPackageName
+	fileType               = 'EXE'
+	silentArgs             = '/S'
+	validExitCodes         = @(0)
+	softwareName           = 'linphone'
+	}
+	
+	# get uninstaller
+	[array]$key = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match $packageArgsUnInstall['softwareName'] } | Select-Object -Property DisplayName, UninstallString
+	if ($key.Count -eq 1) {
+	$key | % { 
+		$packageArgsUnInstall['file'] = "$($_.UninstallString)"
+		Write-Verbose "uninstall old $($_.DisplayName) version: $($_.UninstallString) $packageArgsUnInstall['silentArgs']"
+		Uninstall-ChocolateyPackage @packageArgsUnInstall
+	}
+	} elseif ($key.Count -eq 0) {
+	Write-Verbose "skip uninstall of old $packageArgsUnInstall['softwareName'] version, no installation found"
+	}
+	
+	# install 
+	$packageArgsInstall = @{
+	packageName            = $env:ChocolateyPackageName
+	fileType               = 'EXE'
+	url                    = 'http://www.linphone.org/releases/windows/app/Linphone-4.3.1-win32.exe'
+	checksum               = '2F28E349256BEA2F2A364555854ACE5AAFB6AD2C9F5B208FB5D58A8C9584A847'
+	checksumType           = 'sha256'
+	silentArgs             = '/S'
+	validExitCodes         = @(0)
+	softwareName           = 'linphone'
+	}
+	
+	Install-ChocolateyPackage @packageArgsInstall
+} else {
+ 	Write-Error "A version of linphone is already running, please stop and try again"
 }
-Install-ChocolateyPackage @packageArgs
